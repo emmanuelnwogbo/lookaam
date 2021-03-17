6605d07764b40fbd04d277a0caa02ceef7d388f3
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2021-03-17 01:27:32 +0100

    clean

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Header />
     <div class="landing__jumbotron">
       <figure>
-        <img src="~/assets/landing/introduction.jpg" alt="" />
+        <img src="~/assets/landing/introduction.jpg" alt="">
       </figure>
     </div>
 
